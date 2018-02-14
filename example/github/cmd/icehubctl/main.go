630fa653ffe352a0e45e34a67f3804482b72c550
at15 <at15@outlook.com>
2018-02-13 18:47:06 -0800

    [grpc][example] Add client ping
    - no concrete implementation for client is needed
    - [ ] TODO: dependency is not handled properly in example, CI should
    break if we run `dep ensure -update` in example folder

--- example/github/cmd/icehubctl/main.go
+++ example/github/cmd/icehubctl/main.go
@@ -5,20 +5,58 @@ import (
 	"os"
 
 	"github.com/at15/go.ice/ice"
+	"google.golang.org/grpc"
+	dlog "github.com/dyweb/gommon/log"
 
 	"github.com/at15/go.ice/example/github/pkg/common"
+	mygrpc "github.com/at15/go.ice/example/github/pkg/transport/grpc"
+	"github.com/spf13/cobra"
+	"context"
+	"github.com/at15/go.ice/example/github/pkg/icehubpb"
 )
 
-// TODO: create grpc client and try ping ....
+const (
+	myname = "icehubctl"
+)
+
+var log = dlog.NewApplicationLogger()
+var addr = "localhost:7081"
+var conn *grpc.ClientConn
+var client mygrpc.IceHubClient
+
+var pingCmd = &cobra.Command{
+	Use:   "ping",
+	Short: "ping server",
+	Long:  "Ping server using gRPC",
+	Run: func(cmd *cobra.Command, args []string) {
+		mustCreateClient()
+		if res, err := client.Ping(context.Background(), &icehubpb.Ping{Name: myname}); err != nil {
+			log.Fatalf("failed to ping %v", err)
+		} else {
+			log.Infof("ping finished name is %s", res.Name)
+		}
+	},
+}
 
 func main() {
 	app := ice.New(
-		ice.Name("icehubctl"),
+		ice.Name(myname),
 		ice.Description("Client of IceHub, which is an example GitHub integration service using go.ice"),
-		ice.Version(common.Version()))
+		ice.Version(common.Version()),
+		ice.LogRegistry(log))
 	root := ice.NewCmd(app)
+	root.AddCommand(pingCmd)
 	if err := root.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func mustCreateClient() {
+	var err error
+	conn, err = grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("can't dial %v", err)
+	}
+	client = mygrpc.NewClient(conn)
 }
