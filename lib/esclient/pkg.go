d63dcefc99590e03efb2cc4697459d155d4a0b3f
at15 <at15@outlook.com>
2020-01-13 13:23:34 -0800

    [lib] Remove dockerclient

--- lib/esclient/pkg.go
+++
@@ -1,3 +0,0 @@
-// Package esclient is client for elasticsearch
-// It is an example for httpclient package, it is currently in tree and will be moved to its own repo
-package esclient
--- lib/githubclient/pkg.go
+++
@@ -1,3 +0,0 @@
-// Package githubclient is a client for https://developer.github.com/v3/ (v4 is GraphQL)
-// It is an example for httpclient package, it is currently in tree and will be moved to its own repo
-package githubclient
