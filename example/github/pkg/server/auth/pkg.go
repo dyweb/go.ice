7435ef69fff364447d800c82b791834a8efa78ec
at15 <at15@outlook.com>
2020-01-13 13:53:35 -0800

    [example] Remove github example i.e. icehub #4 #18

--- example/github/pkg/server/auth/pkg.go
+++
@@ -1,7 +0,0 @@
-package auth
-
-import (
-	"github.com/dyweb/go.ice/example/github/pkg/util/logutil"
-)
-
-var log, _ = logutil.NewPackageLoggerAndRegistry()
--- example/github/pkg/server/pkg.go
+++
@@ -1,8 +0,0 @@
-// Package server contains http and grpc server
-package server
-
-import (
-	"github.com/dyweb/go.ice/example/github/pkg/util/logutil"
-)
-
-var log, _ = logutil.NewPackageLoggerAndRegistry()
