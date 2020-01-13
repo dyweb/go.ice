27b2c9d03be63bccbed8ab4194642d6a4e5a3dd8
at15 <at15@outlook.com>
2020-01-13 13:57:46 -0800

    [tracing] Remove old tracing lib

--- ice/tracing/pkg.go
+++
@@ -1,8 +0,0 @@
-// Package tracing wraps opentracing api
-package tracing // import "github.com/dyweb/go.ice/ice/tracing"
-
-import (
-	"github.com/dyweb/go.ice/ice/util/logutil"
-)
-
-var log, _ = logutil.NewPackageLoggerAndRegistry()
